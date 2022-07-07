2e7bab58f3ec069073f9104f1707b37aa918c3d8
Raunak Sharma <98183441+Sharma572@users.noreply.github.com>
2022-07-08 01:08:25 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,43 @@
+/** @format */
+
+function addList(event) {
+  event.preventDefault();
+  // window.history.back();
+
+  let containner = document.querySelector(".todo-list");
+  
+  let val = document.querySelector(".todo-input").value;
+  if (val != "") {
+    let task = document.createElement("li");
+    task.classList.add("todo-item");
+    task.textContent = document.querySelector(".todo-input").value;
+    containner.appendChild(task);
+    document.querySelector(".todo-input").value = "";
+
+    //Complete Button ====>
+
+    let completeButton = document.createElement("button");
+    completeButton.appendChild(document.createTextNode("Complete"));
+    containner.appendChild(completeButton);
+    completeButton.classList.add("complete-btn");
+    let flag = true;
+    completeButton.addEventListener("click", function () {
+      task.style.textDecoration = flag
+        ? "line-through solid rgb(0, 0, 0)"
+        : "none";
+      flag = !flag;
+    });
+
+    // Delete Button ====>
+
+    let deleteButton = document.createElement("button");
+    deleteButton.classList.add("trash-btn");
+    deleteButton.appendChild(document.createTextNode("Delete"));
+    containner.appendChild(deleteButton);
+    deleteButton.addEventListener("click", function () {
+      containner.removeChild(task);
+      containner.removeChild(completeButton);
+      containner.removeChild(deleteButton);
+    });
+  }
+}
